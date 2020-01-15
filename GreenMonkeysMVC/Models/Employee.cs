2a3e6c78f4cf0ebc510be44435e254e0bbfe600e
DylPickle11 <Dylan.Griff11@gmail.com>
2020-01-15 15:00:10 -0600

    Here is boht the tickets

---
+++ GreenMonkeysMVC/Models/Employee.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+namespace GreenMonkeysMVC.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int DepartmentId { get; set; }
+        public string Email { get; set; }
+        public bool IsSupervisor { get; set; }
+        public int ComputerId { get; set; }
+    }
+}
