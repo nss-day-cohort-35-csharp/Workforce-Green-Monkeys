396d986616e00a515e75bacc3d21184536247bae
George Williamson <georgekwilliamson@gmail.com>
2020-01-17 04:49:35 -0800

    employees edit works

--- GreenMonkeysMVC/Controllers/EmployeesController.cs
+++ GreenMonkeysMVC/Controllers/EmployeesController.cs
@@ -34,16 +34,6 @@ namespace GreenMonkeysMVC.Controllers
             return View(allEmployeesWithDepartments);
         }
 
-        /*
-         * 
-         * DepartmentRepository departmentRepo = new DepartmentRepository();
-                        Department singleDepartment = departmentRepo.GetDepartmentById(departmentIdValue);
-          // add to Instructors List
-                    var returnedInstructors = await new InstructorsController(_config).allInstructorsList();
-                    List<Instructor> instructorsList = new List<Instructor>(returnedInstructors);
-                    cohorts.ForEach(c => c.Instructors.AddRange(instructorsList.Where(i => i.CohortId == c.Id)));
-             */
-
         // GET: Employees/Details/5
         public ActionResult Details(int id)
         {
@@ -112,22 +102,45 @@ namespace GreenMonkeysMVC.Controllers
                 return View();
             }
         }
-
         // GET: Employees/Edit/5
         public ActionResult Edit(int id)
         {
-            return View();
+            DepartmentRepository departmentRepo = new DepartmentRepository();
+            var departments = departmentRepo.GetAllDepartments().Select(d => new SelectListItem
+            {
+                Text = d.Name,
+                Value = d.Id.ToString()
+            }).ToList();
+
+            ComputerRepository computerRepo = new ComputerRepository();
+            var computers = computerRepo.GetAvailableComputers().Select(d => new SelectListItem
+            {
+                Text = $"{d.Make} {d.Model}",
+                Value = d.Id.ToString()
+            }).ToList();
+
+            EmployeeRepository employeeRepo = new EmployeeRepository();
+            var employee = employeeRepo.GetEmployeeById(id);
+
+             var viewModel = new EmployeeCreateModel()
+             {
+                  Employee = employee,
+                  Departments = departments,
+                  Computers = computers
+             };
+             return View(viewModel);
         }
+
 
         // POST: Employees/Edit/5
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit(int id, IFormCollection collection)
+        public ActionResult Edit(int id, Employee employee)
         {
             try
             {
-                // TODO: Add update logic here
-
+                EmployeeRepository employeeRepo = new EmployeeRepository();
+                employeeRepo.UpdateEmployee(id, employee);
                 return RedirectToAction(nameof(Index));
             }
             catch
